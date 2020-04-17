988176d710e668c512fcfae53ead8fe8fb559f45
Joao Victor <joaovictorvieira.23@hotmail.com>
2020-04-17 18:34:08 -0300

    feat: add dashboard controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,12 +4,16 @@ const uploadConfig = require('./config/upload');
 
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
 
+routes.get('/spots',SpotController.index);
 routes.post('/spots', upload.single('thumbnail') ,SpotController.store);
+
+routes.get('/dashboard',DashboardController.show);
 
 module.exports = routes;
