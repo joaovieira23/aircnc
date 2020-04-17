146ecc7224bde4c16f7c3b2595ce9511451f4c65
Joao Victor <joaovictorvieira.23@hotmail.com>
2020-04-17 18:07:49 -0300

    feat: add model and spot controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
 
 const routes = express.Router();
 
 routes.post('/sessions', SessionController.store);
+routes.get('/spots', SpotController.store);
 
 module.exports = routes;
