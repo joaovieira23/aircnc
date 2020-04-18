4719dbc2a4a5ea3be6ac9de5e332f08687d9e837
Joao Victor <joaovictorvieira.23@hotmail.com>
2020-04-17 21:27:22 -0300

    chore: add cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +11,7 @@ mongoose.connect('mongodb+srv://aircnc:aircnc@cluster0-hozfd.mongodb.net/semana0
   useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
