3c077f59fc0b86ced36d88642cf2307b57bde4d2
Joao Victor <joaovictorvieira.23@hotmail.com>
2020-04-17 22:56:20 -0300

    feat: configure image url

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -13,6 +14,7 @@ mongoose.connect('mongodb+srv://aircnc:aircnc@cluster0-hozfd.mongodb.net/semana0
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
