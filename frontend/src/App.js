b8dbf7598607a2f8a63f1ed1410943bf28d41ed4
Joao Victor <joaovictorvieira.23@hotmail.com>
2020-04-17 21:28:18 -0300

    feat: store user id in local storage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    
+    const response = await api.post('/sessions', { email });
+
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
@@ -13,12 +26,14 @@ function App() {
           Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL *</label>
           <input 
             type="email" 
             id="email" 
-            placeholder="Seu melhor e-mail" 
+            placeholder="Seu melhor e-mail"
+            value={email} 
+            onChange={event => setEmail(event.target.value)}
           />
 
           <button className="btn" type="submit">Entrar</button>
